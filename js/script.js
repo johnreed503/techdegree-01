3ec7c09d748256bbc5418dd3932563dc36af88fb
John Reed <contact.johnreed@gmail.com>
2020-10-27 21:22:30 -0400

    Added printQuote function
    not working with 'show another quote' button yet

--- js/script.js
+++ js/script.js
@@ -12,57 +12,104 @@ project 1 - A Random Quote Generator
 ***/
 const quotes = [
   {
-    'quote' : 'Give me six hours to chop down a tree and I will spend the first four sharpening the ax',
-    'source' : 'Abraham Lincoln'
+    quote : 'Give me six hours to chop down a tree and I will spend the first four sharpening the ax',
+    source : 'Abraham Lincoln',
   },
   {
-    'quote' : 'These things cannot be explained in detail. From one thing, know ten thousand things.',
-    'source' : 'Miyamoto Musashi',
-    'citation' : 'A Book of Five Rings',
-    'year' : '1965'
+    quote : 'These things cannot be explained in detail. From one thing, know ten thousand things.',
+    source : 'Miyamoto Musashi',
+    citation : 'A Book of Five Rings',
+    year : '1965'
   },
   {
-    'quote' : 'All that you touch you change. All that you change changes you',
-    'source' : 'Octavia Butler',
-    'citation' : 'Parable of the Sower',
-    'year' : '1993'
+    quote : 'All that you touch you change. All that you change changes you',
+    source : 'Octavia Butler',
+    citation : 'Parable of the Sower',
+    year : '1993'
   },
   {
-    'quote' : 'Do not lose time on daily trivialities. Do not dwell on petty detail. For all of these things melt away and drift apart within the obscure traffic of time. Live well and live broadly.',
-    'source' : 'Don Hertzfeldt',
-    'citation' : 'World of Tomorrow',
-    'year' : '2015'
+    quote : 'Do not lose time on daily trivialities. Do not dwell on petty detail. For all of these things melt away and drift apart within the obscure traffic of time. Live well and live broadly.',
+    source : 'Don Hertzfeldt',
+    citation : 'World of Tomorrow',
+    year : '2015'
   },
   {
-    'quote' : 'Deep in the human unconscious is a pervasive need for a logical universe that makes sense. But the real universe is always one step beyond logic',
-    'souce' : 'Frank Herbert',
-    'citation' : 'Dune',
-    'year' : '1965'
+    quote : 'Deep in the human unconscious is a pervasive need for a logical universe that makes sense. But the real universe is always one step beyond logic',
+    source : 'Frank Herbert',
+    citation : 'Dune',
+    year : '1965'
   }
 ];
 
 
-/***
- * `getRandomQuote` function
-***/
 
+/***
+ * `getRandomInt` to use in getRandomQuote
+***/
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 };
 
-function getRandomQuote(array) {
-  let randomQuote = array(getRandomInt(0, array.length))
+/***
+ * `getRandomQuote` function to select random quote from quotes array
+***/
+function getRandomQuote(arr) {
+  let randomQuote = arr[getRandomInt(0, arr.length)]
   return randomQuote
 };
 
-console.log(getRandomInt(0, quotes.length))
+
 /***
  * `printQuote` function
 ***/
+function printQuote(arr) {
+  let html = ''
+  let quote = getRandomQuote(arr)
 
+    //to display if theres a citation and year
+  if (quote.citation !== undefined && quote.year !== undefined) {
+    html += `
+    <p class="quote"> ${quote.quote} </p>
+    <p class="source"> ${quote.source}
+    <span class="citation"> ${quote.citation} </span>
+    <span class="year"> ${quote.year} </span>
+    </p>
+    `
+  }
+  //to display if theres a citation but no year
+  else if (quote.citation !== undefined && quote.year === undefined) {
+    html += `
+    <p class="quote"> ${quote.quote} </p>
+    <p class="source"> ${quote.source}
+    <span class="citation"> ${quote.citation} </span>
+    </p>
+    `
+  }
+  //to display if theres a year but no citation
+  else if (quote.citation === undefined && quote.year !== undefined) {
+    html += `
+    <p class="quote"> ${quote.quote} </p>
+    <p class="source"> ${quote.source}
+    <span class="year"> ${quote.year} </span>
+    </p>
+    `
+  }
+  //to display if theres no citation or year
+  else {
+    html += `
+    <p class="quote"> ${quote.quote} </p>
+    <p class="source"> ${quote.source}
+    </p>
+    `
+  }
+  return html
+  //document.getElementById('quote-box').innerHTML = html;
+};
 
+let test = printQuote(quotes)
+console.log(test)
 
 /***
  * click event listener for the print quote button
