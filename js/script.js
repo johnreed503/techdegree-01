65c1ee4148b52930585632f8006b30e4716b01fc
John Reed <contact.johnreed@gmail.com>
2020-10-28 17:13:15 -0400

    added random_bg_color function
    function changes the background to a random color every time a new quote is selected

--- js/script.js
+++ js/script.js
@@ -142,6 +142,19 @@ function printQuote() {
     </p>
     `
   }
+
+  function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    console.log(bgColor);
+
+    document.body.style.background = bgColor;
+    }
+
+  random_bg_color();
+
   return document.getElementById('quote-box').innerHTML = html;
   //document.getElementById('quote-box').innerHTML = html;
 };
